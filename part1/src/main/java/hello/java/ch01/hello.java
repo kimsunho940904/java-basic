05fff928606fbe783d95cc58c4e75055b942db99
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-01 18:17:00 +0900

    java basic start

---
+++ part1/src/main/java/hello/java/ch01/hello.java
@@ -0,0 +1,7 @@
+package hello.java.ch01;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ part1/src/test/java/hello/java/Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
